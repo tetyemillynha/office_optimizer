b2b03840d97190658316594d9542c3e8b4fa1c0e
Stefhany Santos <stefhany.emilly@hotmail.com>
2025-07-17 22:30:16 -0300

    feat: calcular total calorias

--- fitness.py
+++ fitness.py
@@ -1,6 +1,26 @@
 from alimentos import alimentos
 
 CALORIAS_OBJETIVO = 2000
+MACROS_OBJETIVO = {
+    "carbo": 0.5,
+    "proteina": 0.3,
+    "gordura": 0.2
+}
+
+def calcular_total_calorias(alimentos):
+    total = {
+        "calorias": 0,
+        "carbo": 0,
+        "proteina": 0,
+        "gordura": 0
+    }
+
+    for alimento in alimentos:
+        total["calorias"] += alimento["calorias"]
+        total["carbo"] += alimento["carbo"]
+        total["proteina"] += alimento["proteina"]
+        total["gordura"] += alimento["gordura"]
+    return total
 
 def calcular_calorias(alimento):
     return alimento["calorias"]
